03300e9cd316173c1ffaaa34ab0eca3ef5430b46
Juan Hebert Chable <xmbeat@hotmail.com>
2024-03-31 14:38:00 -0600

    added: Suport for more hashing algorithms in deriveKey function

--- lib/pbkdf2_hmac/pbkdf2_hmac.dart
+++ lib/pbkdf2_hmac/pbkdf2_hmac.dart
@@ -4,14 +4,27 @@ import 'package:cryptography/cryptography.dart';
 
 import '../keys/derivation_artefacts.dart';
 import '../keys/derivation_service.dart';
-
+Map supportedHashAlgorithms = {
+  "sha1":  Sha1(),
+  "sha224":  Sha224(),
+  "sh384":  Sha384(),
+  "sha256":  Sha256(),
+  "sha512":  Sha512()
+};
 /// Currently the only supported key derivation strategy
 /// https://en.wikipedia.org/wiki/Pbkdf2
 class Pbkdf2Hmac implements DerivationService {
   Future<List<int>> deriveKey(
-      {required String passphrase, required DerivationArtefacts artefacts}) async {
+      {required String passphrase, required DerivationArtefacts artefacts, String hashAlgorithm = "sha256"}) async {
+    late HashAlgorithm algorithm;
+    if (supportedHashAlgorithms.containsKey(hashAlgorithm)){
+      algorithm = supportedHashAlgorithms[hashAlgorithm];
+    }
+    else{
+      throw UnsupportedError('$hashAlgorithm is not an algorithm supported');
+    }
     final pbkdf2 = Pbkdf2(
-      macAlgorithm: Hmac(Sha256()),
+      macAlgorithm: Hmac(algorithm),
       iterations: artefacts.iterations,
       bits: artefacts.length * 8,
     );
